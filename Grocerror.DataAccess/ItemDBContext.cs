b0dabf37f8411ed9b78f9c50baa36ef82986196b
Ravindu007 <ravindu0504@gmail.com>
2023-11-03 17:55:15 +0530

    Database Context Creation

---
+++ Grocerror.DataAccess/ItemDBContext.cs
@@ -0,0 +1,33 @@
+﻿using Grocerror.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grocerror.DataAccess
+{
+    internal class ItemDBContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server = localhost; Database = GroceryItems; User Id = sa; Password = Rs@19980504; TrustServerCertificate = true";
+            optionsBuilder.UseSqlServer(connectionString);  
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Item>().HasData(new Item
+            {
+                Id = 1,
+                ItemName = "Milk",
+                ListId = 1,
+                Date = DateTime.Now,
+                Status = ItemStatus.Got
+            });
+        }
+    }
+}
---
+++ Grocerror.DataAccess/Migrations/20231103122357_dataadded.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Grocerror.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class dataadded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Items",
+                columns: new[] { "Id", "Date", "ItemName", "ListId", "Status" },
+                values: new object[] { 1, new DateTime(2023, 11, 3, 17, 53, 57, 284, DateTimeKind.Local).AddTicks(1929), "Milk", 1, 0 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Items",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
